b32ba1629adfd05c4782529c162a0aae7c80102c
gab <74925482+nyutiz@users.noreply.github.com>
2026-07-18 19:53:40 +0200

    Delete build directory

--- build/riscv64gc-unknown-none-elf/release/deps/kernel_rust-1ac333defbe22f16.d
+++
@@ -1,5 +0,0 @@
-/mnt/d/Code/c/NanoOS/build/riscv64gc-unknown-none-elf/release/deps/kernel_rust-1ac333defbe22f16.d: src/lib.rs
-
-/mnt/d/Code/c/NanoOS/build/riscv64gc-unknown-none-elf/release/deps/libkernel_rust-1ac333defbe22f16.a: src/lib.rs
-
-src/lib.rs:
--- build/riscv64gc-unknown-none-elf/release/libkernel_rust.d
+++
@@ -1,1 +0,0 @@
-/mnt/d/Code/c/NanoOS/build/riscv64gc-unknown-none-elf/release/libkernel_rust.a: /mnt/d/Code/c/NanoOS/src/lib.rs
